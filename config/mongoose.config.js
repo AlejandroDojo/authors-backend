5ee632f881cda7c85500d18d392b1534cb93e8a6
AlejandroDojo <ultiimately@gmail.com>
2024-06-14 21:38:16 -0400

    updateData

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/authorsdb")
+const uri = "mongodb+srv://Luis:admin123@backendserver.m9bhizj.mongodb.net/?retryWrites=true&w=majority&appName=backEndServer";
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+mongoose.connect(uri, clientOptions)
     .then(() => console.log("Established a connection to the database"))
     .catch(err => console.log("Something went wrong when connecting to the database", err));
