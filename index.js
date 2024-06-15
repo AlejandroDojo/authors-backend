d25239461c808b6e829e85acc3c21ea47aba18d3
AlejandroDojo <ultiimately@gmail.com>
2024-06-14 22:23:47 -0400

    updateIndex

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const cors = require('cors');
 const ip = require('ip');
 const ipAddress = ip.address();
 const app = express();
-//const AuthorRoute = require('./routes/author.routes')
-//require('./config/mongoose.config');
+console.log(`Server created at ip: ${ipAddress}:8000`);
+const AuthorRoute = require('./routes/author.routes')
+require('./config/mongoose.config');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//AuthorRoute(app);
+AuthorRoute(app);
 app.listen(8000, () => {
     console.log(`Server created at ip: ${ipAddress}:8000`)
     console.log("Listening at Port 8000")
